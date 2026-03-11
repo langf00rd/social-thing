d15732e2c0d4d9078a05ce78d0c6d176d362ee32
langford <langfordquarshie21@gmail.com>
2026-03-11 19:55:02 +0000

    chore: update one signal setup

--- components/onesignal-provider.tsx
+++ components/onesignal-provider.tsx
@@ -1,19 +1,24 @@
 "use client";
 
-import { useAuth } from "@/hooks/use-auth";
 import { useEffect } from "react";
 import OneSignal from "react-onesignal";
+import { useAuth } from "@/hooks/use-auth";
 
 export function OneSignalProvider() {
   const { user } = useAuth();
 
   useEffect(() => {
-    if (!user?.auth_id) return;
-    if (typeof window !== "undefined") {
-      OneSignal.init({
-        appId: process.env.NEXT_PUBLIC_ONESIGNAL_APP_ID || "",
+    const initOneSignal = async () => {
+      await OneSignal.init({
+        appId: process.env.NEXT_PUBLIC_ONESIGNAL_APP_ID!,
       });
-    }
+
+      if (user?.auth_id) {
+        await OneSignal.login(user.auth_id);
+      }
+    };
+
+    initOneSignal();
   }, [user?.auth_id]);
 
   return null;
